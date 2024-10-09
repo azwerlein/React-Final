a0529490c53f2260577ebc89cd03705dfde9d0cf
azwerlein <anon@mail.com>
2024-10-09 09:34:04 -0500

    Change base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/aws-apps/medals-radix-06/",
+  base: "https://azwerlein.github.io/React-Final/",
   plugins: [react()],
 });
